3566e987a2ee9fab5bd28ce362347b491d278e4c
raria_000 <raria_000@Raymond>
2015-10-16 15:54:27 -0700

    Fixed bugs with the main of the program

--- Project1/src/Project1.java
+++ Project1/src/Project1.java
@@ -1,121 +1,104 @@
 
 import java.util.Scanner;
 
-
 public class Project1 {
-    public static void main (String []args)
-    {
+
+    public static void main(String[] args) {
         BinarySearchTree bsTree = new BinarySearchTree();
         Scanner keyboardInput = new Scanner(System.in);
         String usersChoice = null;
-        /*bsTree.add(51, bsTree.getRoot());
-        bsTree.add(29, bsTree.getRoot());
-        bsTree.add(68, bsTree.getRoot());
-        bsTree.add(90, bsTree.getRoot());
-        bsTree.add(36, bsTree.getRoot());
-        bsTree.add(40, bsTree.getRoot());
-        bsTree.add(22, bsTree.getRoot());
-        bsTree.add(59, bsTree.getRoot());
-        bsTree.add(44, bsTree.getRoot());
-        bsTree.add(99, bsTree.getRoot());
-        bsTree.add(1, bsTree.getRoot());
-        bsTree.add(77, bsTree.getRoot());
-        bsTree.add(60, bsTree.getRoot());
-        bsTree.add(27, bsTree.getRoot());
-        bsTree.add(83, bsTree.getRoot());
-        bsTree.add(15, bsTree.getRoot());
-        bsTree.add(75, bsTree.getRoot());
-        bsTree.add(3, bsTree.getRoot());
-        */ 
+        String userInput = null;
+        String []tokenizedStr = null; 
         
-        bsTree.add(60000, bsTree.getRoot());
-        bsTree.add(1, bsTree.getRoot());
-        bsTree.add(2, bsTree.getRoot());
-        bsTree.add(3, bsTree.getRoot());
-        bsTree.add(4, bsTree.getRoot());
-        bsTree.add(5, bsTree.getRoot());
-        bsTree.add(60, bsTree.getRoot());
-        bsTree.add(600, bsTree.getRoot());
-        bsTree.add(6000, bsTree.getRoot());
-        bsTree.add(60000, bsTree.getRoot());
+        System.out.println("Please enter initial sequence of characters: ");
+        userInput = keyboardInput.nextLine();
+        tokenizedStr = userInput.split(" ");
+        for(int i = 0; i < tokenizedStr.length; i++)
+        {
+            bsTree.add(Integer.parseInt(tokenizedStr[i]), bsTree.getRoot());
+        }
         
-        
+
         System.out.print("Pre-order: ");
         bsTree.preOrderTraversal(bsTree.getRoot());
-         System.out.print("\nIn-order: ");
+        System.out.print("\nIn-order: ");
         bsTree.inOrderTraversal(bsTree.getRoot());
-         System.out.print("\nPost-order: ");
+        System.out.print("\nPost-order: ");
         bsTree.postOrderTraversal(bsTree.getRoot());
-        
-        while(true)
-        {
+
+        while (true) {
             System.out.print("\nCommand? ");
             usersChoice = keyboardInput.nextLine();
             
-            if(usersChoice.charAt(0) == 'I')
+           
+            if(usersChoice.length() == 0)
+            {
+                //No command entered. Simply reloop
+            }
+            else if (usersChoice.charAt(0) == 'I') 
             {
                 //Insert a value
                 int userNum = Integer.parseInt(bsTree.stringTokenizer(usersChoice));
                 bsTree.add(userNum, bsTree.getRoot());
                 System.out.print("In-Order: ");
                 bsTree.inOrderTraversal(bsTree.getRoot());
-            }
-            else if(usersChoice.charAt(0) == 'D')
+            } 
+            else if (usersChoice.charAt(0) == 'D')
             {
                 //Delete a value
                 int userNum = Integer.parseInt(bsTree.stringTokenizer(usersChoice));
                 bsTree.remove(userNum, bsTree.getRoot(), null);
                 System.out.print("In-Order: ");
                 bsTree.inOrderTraversal(bsTree.getRoot());
-            }
-            else if(usersChoice.charAt(0) == 'P')
+            } 
+            else if (usersChoice.charAt(0) == 'P') 
             {
                 //find prodessor
                 int userNum = Integer.parseInt(bsTree.stringTokenizer(usersChoice));
                 int predessor = bsTree.getPredecessor(userNum);
-                if(predessor == -99)
+                if (predessor == -99)
                 {
-                    System.out.println("Error: " + userNum +" is either not in list or does not have predessor");
+                    System.out.println("Error: " + userNum + " is either not in list or does not have predessor");
                 }
-                else
+                else 
                 {
-                    System.out.println(bsTree.getPredecessor(userNum));
+                    System.out.print(bsTree.getPredecessor(userNum));
                 }
-                
-            }
-            else if(usersChoice.charAt(0) == 'S')
+
+            } 
+            else if (usersChoice.charAt(0) == 'S')
             {
                 //find sucessor
                 int userNum = Integer.parseInt(bsTree.stringTokenizer(usersChoice));
                 int sucessor = bsTree.getSucessor(userNum);
-                if(sucessor == -99)
+                if (sucessor == -99)
                 {
-                    System.out.println("Error: " + userNum +" is either not in list or does not have sucessor");
-                }
+                    System.out.println("Error: " + userNum + " is either not in list or does not have sucessor");
+                } 
                 else
                 {
-                    System.out.println(sucessor);
+                    System.out.print(sucessor);
                 }
-            }
-            else if(usersChoice.charAt(0) == 'E')
+            } 
+            else if (usersChoice.charAt(0) == 'E') 
             {
                 //exit program
-                System.out.println("Thank you for using my project");
+                System.out.println("Thank you for using my program");
                 break;
-            }
-            else if(usersChoice.charAt(0) == 'H')
-            {
+            } 
+            else if (usersChoice.charAt(0) == 'H') {
                 //Display menu
                 bsTree.showMenu();
-            }
+            } 
             else
             {
                 //Error message
                 System.out.println("Error: Command not found");
                 System.out.println("Type H for list of commands");
             }
-            
         }
+
     }
-        
 }
+
+
