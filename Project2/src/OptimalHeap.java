00d2e35bd32e09473a7e598ba4fa29714db3548d
RaymondArias <rarias2010@yahoo.com>
2015-11-02 19:46:01 -0800

    Fixed bugs and added comments to classes and methods.

--- Project2/src/OptimalHeap.java
+++ Project2/src/OptimalHeap.java
@@ -1,10 +1,15 @@
+/**
+ * This class creates a heap
+ * using the optimal method;
+ * @author Raymond Arias
+ *
+ */
 import java.util.Arrays;
-import java.util.Random;
 
 public class OptimalHeap {
-	private int []heapArray;
-	private int dataCount;
-	private int numberOfSwaps;
+	private int []heapArray; 	//Array that holds heap
+	private int dataCount;		//Number of data held in heap
+	private int numberOfSwaps;	//Number of swaps required to build heap
 	
 	public OptimalHeap()
 	{
@@ -13,6 +18,11 @@ public class OptimalHeap {
 		dataCount = 0;
 		numberOfSwaps = 0;
 	}
+	/**
+	 * Adds data to heap, if data is 
+	 * currently not in heap
+	 * @param element
+	 */
 	public void add(int element)
 	{
 		if(contains(element))
@@ -20,35 +30,30 @@ public class OptimalHeap {
 		heapArray[dataCount] = element;
 		dataCount++;
 	}
+	/**
+	 * Builds heap using the optimal method, reheapify upward.
+	 * Also, keeps track of swaps needed
+	 * 
+	 */
 	public void reheapifyUpward()
 	{
 		int i = dataCount - 1;
 		int parent = (int) Math.floor((i - 1) / 2);
 		while(i > -1)
 		{
-			if(2 * parent + 2 > dataCount - 1)
+			//Double array size if the child node is out of bounds
+			if(2 * parent + 2 >= heapArray.length)
 			{
-				if(heapArray[parent] < heapArray[2 * parent + 1])
-				{
-					int tempNum;
-					int newRoot;
-					if(heapArray[parent] < heapArray[2 * parent + 1])
-					{
-						tempNum = heapArray[2 * parent + 1];
-						heapArray[2 * parent + 1] = heapArray[parent];
-						heapArray[parent] = tempNum;
-						newRoot = 2 * parent + 1;
-						numberOfSwaps ++;
-						swapRoot(newRoot);
-					}
-					
-				}
+				doubleArraySize();
 				
 			}
-			else if(heapArray[parent] < heapArray[2 * parent + 1] || heapArray[parent] < heapArray[2 * parent + 2])
+			//If either child is greater than the parent
+			if(heapArray[parent] < heapArray[2 * parent + 1] || heapArray[parent] < heapArray[2 * parent + 2])
 			{
 				int tempNum;
 				int newRoot;
+				//If the left child is larger than parent and right child
+				//swap with left child
 				if(heapArray[parent] < heapArray[2 * parent + 1] 
 						&& heapArray[2 * parent + 1] > heapArray[2 * parent + 2])
 				{
@@ -58,6 +63,7 @@ public class OptimalHeap {
 					newRoot = 2 * parent + 1;
 					numberOfSwaps ++;
 				}
+				//Swap with right child
 				else
 				{
 					tempNum = heapArray[2 * parent + 2];
@@ -73,6 +79,11 @@ public class OptimalHeap {
 			parent = (int) Math.floor((i - 1) / 2);
 		}
 	}
+	/**
+	 * Used to swap child and parent node data,
+	 * method also keeps track of swaps
+	 * @param newRoot
+	 */
 	public void swapRoot(int newRoot)
 	{
 		if(2 * newRoot + 2  > dataCount - 1)
@@ -87,6 +98,7 @@ public class OptimalHeap {
 				heapArray[2 * newRoot + 1] = heapArray[newRoot];
 				heapArray[newRoot] = tempNum;
 				newRoot = 2 * newRoot + 1;
+				numberOfSwaps ++;
 			}
 			//Right child is larger so swap heapArray[newRoot] with right child
 			else
@@ -95,17 +107,31 @@ public class OptimalHeap {
 				heapArray[2 * newRoot + 2] = heapArray[newRoot];
 				heapArray[newRoot] = tempNum;
 				newRoot = 2 * newRoot + 2;
+				numberOfSwaps ++;
 				
 			}
-			numberOfSwaps ++;
-			if(2 * newRoot + 2 > dataCount - 1)
-				return;
+			
+			if(2 * newRoot + 2 >= heapArray.length)
+			{
+				doubleArraySize();
+			}
 		}
 	}
+	/**
+	 * Return number of swaps needed
+	 * to build heap
+	 * @return
+	 */
 	public int getNumOfSwaps()
 	{
 		return numberOfSwaps;
 	}
+	/**
+	 * Boolean method which returns true
+	 * if data is in heap; else it returns false
+	 * @param element
+	 * @return
+	 */
 	public boolean contains(int element)
 	{
 		for (int i = 0; i < dataCount; i++)
@@ -115,12 +141,19 @@ public class OptimalHeap {
 		}
 		return false;
 	}
+	/**
+	 * Removes root from heap and reheapifies heap
+	 */
 	public void remove()
 	{
 		int root = 0;
 		heapArray[root] = heapArray[dataCount -1];
 		heapArray[dataCount - 1] = -1;
 		dataCount--;
+		if(2 * root + 2 >= heapArray.length)
+		{
+			doubleArraySize();
+		}
 		while (heapArray[root] < heapArray[2 * root + 1] || heapArray[root] < heapArray[2 * root + 1])
 		{
 			//left child is larger so swap heapArray[newRoot] and left child
@@ -140,7 +173,34 @@ public class OptimalHeap {
 				root = 2 * root + 2;
 				
 			}
+			if(2 * root + 2 >= heapArray.length)
+			{
+				doubleArraySize();
+			}
 		}
 	}
+	/**
+	 * Doubles array size, used to
+	 * verify array index out of bounds exception
+	 * is not thrown
+	 */
+	private void doubleArraySize()
+	{
+		int []tempArray = heapArray;
+		heapArray = Arrays.copyOf(tempArray, 2 * tempArray.length);
+		tempArray = null;
+	}
+	/**
+	 * Displays first ten values in heap
+	 */
+	public void displayFirstTen()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.print(heapArray[i] + ", ");
+		}
+		
+	}
+
 
 }
--- Project2/src/Project2.java
+++ Project2/src/Project2.java
@@ -1,7 +1,23 @@
+/**
+ * Main class for program.
+ * Gives users two options
+ * Choice 1: Build 20 heap using both methods with 
+ * randomly generated values and compare average number of
+ * swaps need to build heaps
+ * Choice 2: Build a heap using both methods, output first ten values,compare number of swaps
+ * needed, delete 10 values, and output new first ten values
+ */
+
+
 import java.util.*;
 
 public class Project2 {
 	public Project2(){}
+	
+	/**
+	 * Menu for program, which ask user to choose between 
+	 * two options
+	 */
 	public void menu()
 	{
 		int choice;
@@ -9,7 +25,7 @@ public class Project2 {
 		System.out.println("Please select how to test the program:");
 		System.out.println("(1) 20 sets of 100 randomly generated integers");
 		System.out.println("(2) Fixed integer values 1-100");
-		System.out.println("Enter choice:");
+		System.out.print("Enter choice: ");
 		choice = userInput.nextInt();
 		
 		if(choice == 1)
@@ -20,13 +36,19 @@ public class Project2 {
 		else if (choice == 2)
 		{
 			//Fixed Integers
+			fixedValueInsertion();
 		}
 		else
 		{
 			//error state
+			System.out.println("Error");
 		}
 		
 	}
+	/**
+	 * Choice one: build 20 randomly generated heaps
+	 * with both methods and compare the average number of swaps
+	 */
 	public void randomSetAverage()
 	{
 		double seqHeapAverage = 0.0;
@@ -58,9 +80,44 @@ public class Project2 {
 		
 	
 	}
+	/**
+	 * Choice 2: Build a heap using both methods, output first ten values,compare number of swaps
+	 * needed, delete 10 values, and output new first ten values
+	 */
+	public void fixedValueInsertion()
+	{
+		Heap seqHeap = new Heap();
+		OptimalHeap optHeap = new OptimalHeap();
+		for(int i = 0; i < 100; i++)
+		{
+			seqHeap.add(i + 1);
+			optHeap.add(i + 1);
+		}
+		System.out.print("Heap built using series of insertions: " );
+		seqHeap.displayFirstTen();
+		System.out.println("...\nNumber of swaps: " +seqHeap.getNumOfSwaps());
+		System.out.print("Heap after 10 removals: ");
+		for(int i = 0; i < 10; i++)
+		{
+			seqHeap.remove();
+		}
+		seqHeap.displayFirstTen();
+		
+		optHeap.reheapifyUpward();		
+		System.out.println();
+		System.out.print("\nHeap built using optimal method: ");
+		optHeap.displayFirstTen();
+		System.out.println("\nNumber of swaps: " + optHeap.getNumOfSwaps());
+		for(int i = 0; i < 10; i++)
+		{
+			optHeap.remove();
+		}
+		System.out.print("Heap after 10 removals: ");
+		optHeap.displayFirstTen();
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Project2 output = new Project2();
 		output.menu();
 		
